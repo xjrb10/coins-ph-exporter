c89bc12ef201e03254c551a1d402b4aea3f80bf1
xjrb10 <45172322+xjrb10@users.noreply.github.com>
2022-04-20 10:46:10 +0800

    change default port

--- app/app.py
+++ app/app.py
@@ -75,4 +75,4 @@ def symbol_metrics(symbols: str):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=os.getenv("HTTP_SERVER_PORT", 9798))
+    app.run(host="0.0.0.0", port=os.getenv("HTTP_SERVER_PORT", 9696))
